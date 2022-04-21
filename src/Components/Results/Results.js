f9527f1381734cd6947136389937ac34ccb1bcb4
Rupinder Kaushal <89014750+Rupinderkaushal@users.noreply.github.com>
2022-04-21 09:40:54 +0530

    gog

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,76 +1,78 @@
 import React from 'react';
-import {Button, Table, Tag, Space} from 'antd';
+import { Button, Table, Tag, Space } from 'antd';
 
 import "../scheduled race/Scheduled.css";
 const Results = () => {
-    const columns = [
-        {
-          title: 'Event',
-          dataIndex: 'event',
-          key: 'event',
-         },
-         {
-            title: 'Ran On',
-            dataIndex: 'ranon',
-            key: 'ranon',
-          },
-        {
-          title: 'Location',
-          dataIndex: 'location',
-          key: 'location',
-        },
-        {
-          title: 'Pecking Order',
-          dataIndex: 'order',
-          key: 'order',
-        },
-        {
-            title: 'Distance',
-            dataIndex: 'distance',
-            key: 'distance',
-          },
-          {
-            title: 'Terrain',
-            dataIndex: 'terrain',
-            key: 'terrain',
-          },
-          {
-            title: 'Entry Fee',
-            dataIndex: 'entryfee',
-            key: 'entryfee',
-          },
-          {
-            title: 'PrizePool',
-            dataIndex: 'pool',
-            key: 'pool',
-          },
-        {
-          title: 'Replay',
-          key: 'Replay',
-        
-        },
-      ];
-      const data =[]
+  const columns = [
+    {
+      title: 'Event',
+      dataIndex: 'event',
+      key: 'event',
+    },
+    {
+      title: 'Ran On',
+      dataIndex: 'ranon',
+      key: 'ranon',
+    },
+    {
+      title: 'Location',
+      dataIndex: 'location',
+      key: 'location',
+    },
+    {
+      title: 'Pecking Order',
+      dataIndex: 'order',
+      key: 'order',
+    },
+    {
+      title: 'Distance',
+      dataIndex: 'distance',
+      key: 'distance',
+    },
+    {
+      title: 'Terrain',
+      dataIndex: 'terrain',
+      key: 'terrain',
+    },
+    {
+      title: 'Entry Fee',
+      dataIndex: 'entryfee',
+      key: 'entryfee',
+    },
+    {
+      title: 'PrizePool',
+      dataIndex: 'pool',
+      key: 'pool',
+    },
+    {
+      title: 'Replay',
+      key: 'Replay',
+
+    },
+  ];
+  const data = []
   return (
     <div>
-    <div className='btn-div'>
-        <Button type='primary'  style={{fontWeight:"bolder",fontStyle:"italic"}}><p><span><label style={{color:"yellow",marginRight:"5px"}}>0</label><label id='races' >RACES</label></span> <span><label style={{color:"green",marginLeft:"5px",marginRight:"5px"}}>$0</label><label id="money text" >WON</label></span> </p></Button>
+      <div className='btn-div'>
+        <Button type='primary' style={{ fontWeight: "bolder", fontStyle: "italic" }}><p><span><label style={{ color: "yellow", marginRight: "5px" }}>0</label><label id='races' >RACES</label></span> <span><label style={{ color: "green", marginLeft: "5px", marginRight: "5px" }}>$0</label><label id="money text" >WON</label></span> </p></Button>
         <Button type='primary' >Refresh</Button>
+      </div>
+      <div className='table-div'>
+        <Table columns={columns}
+          pagination={false}
+          dataSource={data}
+          scroll={{ x: 1004, y: 500 }}
+
+          onRow={(record, rowIndex) => {
+            console.log("record", record)
+            console.log("rowindex", rowIndex)
+            return {
+              onClick: () => { console.log("clicked row", rowIndex) }
+            }
+          }}
+        />
+      </div>
     </div>
-    <div className='table-div'>
-    <Table columns={columns}
-    pagination= {false}
-     dataSource={data} 
-      onRow={(record, rowIndex) => {
-        console.log("record",record)
-        console.log("rowindex",rowIndex)
-        return{
-          onClick:()=>{console.log("clicked row",rowIndex)}
-        }
-      }}
-    />
-    </div>
-</div>
   )
 }
 
--- src/Components/enterRace/EnterRace.js
+++ src/Components/enterRace/EnterRace.js
@@ -1,101 +1,101 @@
 import React from 'react';
 import "./EnterRace.css";
-import {Button, Table, Tag, Space} from 'antd';
-import {GoSettings} from 'react-icons/go'
+import { Button, Table, Tag, Space } from 'antd';
+import { GoSettings } from 'react-icons/go'
 const EnterRace = () => {
-    const columns = [
-        {
-          title: 'Event',
-          dataIndex: 'event',
-          key: 'event',
-         },
-        {
-          title: 'Location',
-          dataIndex: 'location',
-          key: 'location',
-        },
-        {
-          title: 'Pecking Order',
-          dataIndex: 'order',
-          key: 'order',
-        },
-        {
-            title: 'Distance',
-            dataIndex: 'distance',
-            key: 'distance',
-          },
-          {
-            title: 'Terrain',
-            dataIndex: 'terrain',
-            key: 'terrain',
-          },
-          {
-            title: 'Entry Fee',
-            dataIndex: 'entryfee',
-            key: 'entryfee',
-          },
-          {
-            title: 'PrizePool',
-            dataIndex: 'pool',
-            key: 'pool',
-          },
-        {
-          title: 'Capacity',
-          key: 'capacity',
-        
-        },
-      ];
-      
-      const data = [
-        {
-          key: '1',
-          event: 'John Brown',
-          order: 'A',
-          location: 'New York No. 1 Lake Park',
-          distance:"100m",
-          terrain:"Terr",
-          entryfee:"$52"
-        },
-        {
-          key: '2',
-          event: 'Jim Green',
-          order: 'B',
-          location: 'London No. 1 Lake Park',
-          distance:"200m",
-          terrain:"Terr",
-          entryfee:"$2"
-            
-        },
-        {
-          key: '3',
-          event: 'Joe Black',
-          order: 'C',
-          location: 'Sidney No. 1 Lake Park',
-          distance:"100m",
-          terrain:"Terr",
-          entryfee:"$25"
-        },
-      ];
+  const columns = [
+    {
+      title: 'Event',
+      dataIndex: 'event',
+      key: 'event',
+    },
+    {
+      title: 'Location',
+      dataIndex: 'location',
+      key: 'location',
+    },
+    {
+      title: 'Pecking Order',
+      dataIndex: 'order',
+      key: 'order',
+    },
+    {
+      title: 'Distance',
+      dataIndex: 'distance',
+      key: 'distance',
+    },
+    {
+      title: 'Terrain',
+      dataIndex: 'terrain',
+      key: 'terrain',
+    },
+    {
+      title: 'Entry Fee',
+      dataIndex: 'entryfee',
+      key: 'entryfee',
+    },
+    {
+      title: 'PrizePool',
+      dataIndex: 'pool',
+      key: 'pool',
+    },
+    {
+      title: 'Capacity',
+      key: 'capacity',
+
+    },
+  ];
+
+  const data = [
+    {
+      key: '1',
+      event: 'John Brown',
+      order: 'A',
+      location: 'New York No. 1 Lake Park',
+      distance: "100m",
+      terrain: "Terr",
+      entryfee: "$52"
+    },
+    {
+      key: '2',
+      event: 'Jim Green',
+      order: 'B',
+      location: 'London No. 1 Lake Park',
+      distance: "200m",
+      terrain: "Terr",
+      entryfee: "$2"
+
+    },
+    {
+      key: '3',
+      event: 'Joe Black',
+      order: 'C',
+      location: 'Sidney No. 1 Lake Park',
+      distance: "100m",
+      terrain: "Terr",
+      entryfee: "$25"
+    },
+  ];
   return (
-    <div>
-        <div className='btn-div'>
-            <Button type='primary' >Refresh</Button>
-            {/* <Button type='primary' danger><GoSettings style={{marginRight:"5px"}}/>Filter</Button> */}
-        </div>
-        <div className='table-div'>
+    <div className="main-wrapper">
+      <div className='btn-div'>
+        <Button type='primary' >Refresh</Button>
+        {/* <Button type='primary' danger><GoSettings style={{marginRight:"5px"}}/>Filter</Button> */}
+      </div>
+      <div className='table-div'>
         <Table columns={columns}
-        pagination= {false}
-         dataSource={data} 
+          pagination={false}
+          dataSource={data}
           onRow={(record, rowIndex) => {
-            console.log("record",record)
-            console.log("rowindex",rowIndex)
-            return{
-              onClick:()=>{console.log("clicked row",rowIndex)}
+            console.log("record", record)
+            console.log("rowindex", rowIndex)
+            return {
+              onClick: () => { console.log("clicked row", rowIndex) }
             }
           }}
-          scroll={{ x: 1004 ,y:500 }}
+          scroll={{ x: 1004, y: 500 }}
         />
-        </div>
+      </div>
     </div>
   )
 }
